acb555bb27d00cfc89bdcfa243b689ae3bd222b8
Laura Jürgensmeier <laura.juergensmeier@gmail.com>
2023-10-10 13:46:17 +0200

    feat: landing background added

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Logo from '../components/Logo';
+import "./Landing.css"
 
 export default function Landing() {
     return (
       <div id="landing-page">
         <Logo/>
+        <div id="landing-background"/>
       </div>
     )
   }
